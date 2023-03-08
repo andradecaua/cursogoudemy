b177b58bcfecfae0f2cf5a68225568d97a13535d
Cauã Andrade <cauasampaiovolei@gmail.com>
2023-03-08 18:17:06 -0300

    Atualizando

--- 15-metodos/metodos.go
+++ 15-metodos/metodos.go
@@ -8,11 +8,24 @@ type usuario struct {
 }
 
 func (u usuario) salvar() {
-	fmt.Printf("Salvando os dados do Usuário %s no banco de dados", u.nome)
+	fmt.Printf("Salvando os dados do Usuário %s no banco de dados\n", u.nome)
+}
+
+func (u usuario) maiorDeIdade() bool {
+	return u.idade >= 18
+}
+
+func (u *usuario) fazerAniversario() {
+	u.idade++
 }
 
 func main() {
 	usuario1 := usuario{"Cauã", 20}
 	fmt.Println(usuario1)
 	usuario1.salvar()
+	maiorDeIdade := usuario1.maiorDeIdade()
+	fmt.Println(maiorDeIdade)
+
+	usuario1.fazerAniversario()
+	fmt.Println(usuario1.idade)
 }
